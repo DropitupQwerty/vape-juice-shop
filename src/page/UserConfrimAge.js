fdaf1e8cb19c6f3c0b95e4da71a23d361ed62d90
Jacob Allen <vjacoballen3020@gmail.com>
2022-12-14 13:32:47 +0800

    No button

--- src/page/UserConfrimAge.js
+++ src/page/UserConfrimAge.js
@@ -3,9 +3,22 @@ import React from 'react';
 import global from '../styles/global';
 import DrawerAppBar from '../components/navbar';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import WarningIcon from '@mui/icons-material/Warning';
 
 export default function UserConfrimAge() {
   const navigate = useNavigate();
+  const [message, setMessage] = useState();
+  const [isAdult, setIsAdult] = useState(true);
+
+  const handleNo = () => {
+    setMessage(`Are you 21 and above? . This website contains adult
+    material and is only suitable for those 21 years older.`);
+  };
+
+  useEffect(() => {
+    handleNo();
+  }, [isAdult]);
 
   return (
     <div>
@@ -25,40 +38,66 @@ export default function UserConfrimAge() {
             }}
           >
             <div>
-              <Typography
-                variant="h4"
-                fontWeight="bold"
-                sx={{ margin: '10px 0' }}
-              >
-                ADULTS ONLY (21+)
-              </Typography>
-
-              <Typography variant="h5">
-                <b>Are you 21 and above?</b> . This website contains adult
-                material and is only suitable for those 21 years older.
-              </Typography>
+              {isAdult ? (
+                <div>
+                  <Typography
+                    variant="h4"
+                    fontWeight="bold"
+                    sx={{ margin: '10px 0' }}
+                  >
+                    ADULTS ONLY (21+)
+                  </Typography>
+                  <Typography variant="h5">{message}</Typography>
+                </div>
+              ) : (
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                  }}
+                >
+                  <WarningIcon sx={{ fontSize: '100px', color: '#B33A3A' }} />
+                  <Typography variant="h4" color="#B33A3A" textAlign="justify">
+                    (21+) Sorry! This website contains adult material and you
+                    are not allowed to view this website
+                  </Typography>
+                </div>
+              )}
             </div>
 
-            <div>
-              <Button
-                sx={{ ...global.btnPrimary }}
-                fullWidth
-                onClick={() => {
-                  navigate('/shop');
-                }}
-              >
-                Yes
-              </Button>
+            {isAdult ? (
+              <div>
+                <Button
+                  sx={{ ...global.btnPrimary }}
+                  fullWidth
+                  onClick={() => {
+                    navigate('/shop');
+                  }}
+                >
+                  Yes
+                </Button>
+                <Button
+                  sx={{ ...global.btnSecondary }}
+                  fullWidth
+                  onClick={() => {
+                    setIsAdult(false);
+                  }}
+                >
+                  No
+                </Button>
+              </div>
+            ) : (
               <Button
                 sx={{ ...global.btnSecondary }}
-                fullWidth
+                variant="outlined"
                 onClick={() => {
                   window.location.replace('https://www.google.com/');
                 }}
               >
-                No
+                Exit
               </Button>
-            </div>
+            )}
           </Paper>
         </Dialog>
       </div>
